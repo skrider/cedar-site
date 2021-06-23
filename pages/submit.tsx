781951bf3744d2abdcad17f7258715c881847885
Caelin Sutch <caelinsutch@gmail.com>
2021-06-23 13:27:45 -0700

    Add logo and folder

---
+++ pages/submit.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type SubmitFormData = {
+  comment: string;
+  name: string;
+
+}
+
+const Submit: React.FC = () => {
+  // const { register, handleSubmit} = use
+
+  return (
+
+  )
+}
+
+export default Submit;
