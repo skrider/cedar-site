9b70780ed8ddb1325b0a600be2dbceb1513910aa
Caelin Sutch <caelinsutch@gmail.com>
2021-06-23 13:18:36 -0700

    Eslint and typescript stuff

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'prettier',
+  ],
+  rules: {
+    indent: [2, 2, { SwitchCase: 1 }],
+    "react/prop-types": 0,
+  },
+};
